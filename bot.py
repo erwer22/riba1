bfbd1eaa49bcdeb0adc70d820e11ec7277cc87a6
erwer22 <164935921+erwer22@users.noreply.github.com>
2024-07-11 17:56:11 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,33 +1,33 @@
-import config
-import telebot
-from random import choice
+import telebot # библиотека telebot
+from config import token # импорт токена
 
+bot = telebot.TeleBot(token) 
 
-bot = telebot.TeleBot(config.token)
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'Добро пожаловать!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
 
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Привет! Я бот для управления чатом.")
 
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
-    
-@bot.message_handler(commands=['info'])
-def send_info(message):
-    bot.reply_to(message,"""я рибабот привет я повтарюшка рибка""")
+ 
 
+@bot.message_handler(commands=['ban'])
+def ban_user(message):
+    if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
+        chat_id = message.chat.id # сохранение id чата
+         # сохранение id и статуса пользователя, отправившего сообщение
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status 
+         # проверка пользователя
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Невозможно забанить администратора.")
+        else:
+            bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
+            bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
+    else:
+        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
 
-@bot.message_handler(commands=['joke'])
-def joke_handler(message):
-    joke = choice(["Две вещи не стареют — черный юмор и невакцинированные дети", "Акробат умер на батуте, но еще какое-то время продолжал радовать публику.","Однорукий человек заплакал, увидев магазин «секонд-хенд»."])
-    bot.reply_to(message, joke)
-
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
-
-
-bot.infinity_polling()
+bot.infinity_polling(none_stop=True)
